c868b06db7859e7925cda39b227804f7530d6abb
Jacob Weinbren <jacobweinbren@gmail.com>
2024-02-04 19:36:56 +0000

    Updated url

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	site: "https://housing.kafkesque.blog",
+	site: "https://environment.kafkesque.blog",
 });
